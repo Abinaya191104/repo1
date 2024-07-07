3ea9110c4fef3723b3c516236feca24f9480ac40
Abinaya191104 <abinayac1119@gmail.com>
2024-07-07 16:58:46 +0530

    basic files

---
+++ folder1/demo.js
@@ -0,0 +1,41 @@
+// p1
+/*
+var price=100;
+var product="kurtitop";
+var tax=20
+console.log(product);
+var total=price+tax
+console.log("total : " +total )
+*/
+
+//p2
+/*
+var fname="banana";
+var count=7;
+var price=50;
+var tot= count*price;
+
+console.log(fname);
+console.log(tot)
+*/
+
+//p3
+/*
+function add(a,b)
+{
+    console.log(a+b)
+}
+
+add(2,3)
+*/
+
+//p4
+var a=7
+var b=1
+if(a>b)
+{
+    console.log("a is big")
+}
+else{
+    console.log("b is big")
+}
